04d9fd2777acc3bed9cc5ba6cb005e3ad46ef517
RIOABLAS <r.ablas.514023@umindanao.edu.ph>
2026-02-23 14:01:12 +0800

    Added RNG to formhandler.php and added a new question to index.php

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -3,6 +3,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $name = htmlspecialchars($_POST["name"]);
     $image = $_FILES["image"];
+
+    if (empty($name)) {
+        exit();
+    }
+
+    $rating = rand(0,100);
+    if ($rating >= 50) {
+        $output = "Congratulations, $name! Your femboy spectrum rating is $rating%. You are a certified femboy!";
+    } else {
+        $output = "Not a femboy.";
+
+    }
     
+    header("Location: ../index.php");
+}
+else {
     header("Location: ../index.php");
 }
--- index.php
+++ index.php
@@ -8,14 +8,12 @@
 
 <h1>WELCOME TO THE FEMBOY SPECTRUM ASSESSMENT (FSA)</h1>
 
-
-
 <form action="includes/formhandler.php" method="post" enctype="multipart/form-data">
-    <label for="name">What is your name?</label>
-    <input type="text" id="name" name="name" required>
+    <label for="name">What is your name?</label><br>
+    <input type="text" id="name" name="name" required><br><br>
 
-    <label for="image">Can I have ur cute selfie photo?</label>
-    <input type="file" name="image" required>
+    <label for="image">Can I have ur cute selfie photo?</label><br>
+    <input type="file" name="image" required><br><br>
 
     <button type="submit" name="submit">Analyze Image</button>
     
